9809cda317420b71b4119abdc8f0b8895ecd5afb
OhYeah68 <sylv.byl@gmail.com>
2022-04-24 16:20:09 +0200

    Updates

---
+++ precache-manifest.9444bbc7380038b8d81338d965dece6f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2c4817248851d39de8dd9da60b1ecf3",
+    "url": "/VotingDapp/index.html"
+  },
+  {
+    "revision": "bbbb7faa3a75b19f0d76",
+    "url": "/VotingDapp/static/css/main.f05a91df.chunk.css"
+  },
+  {
+    "revision": "2b114c7c77046943f827",
+    "url": "/VotingDapp/static/js/2.0b4bcc8e.chunk.js"
+  },
+  {
+    "revision": "bbbb7faa3a75b19f0d76",
+    "url": "/VotingDapp/static/js/main.7ac3cf96.chunk.js"
+  },
+  {
+    "revision": "c08e45cfda69d54b5868",
+    "url": "/VotingDapp/static/js/runtime-main.874071d8.js"
+  }
+]);
